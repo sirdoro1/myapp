b8f43e4e8a74472d7ec7b9f8862466b13580cafe
Oghenedoro David Agbroko <sirdoro1@gmail.com>
2023-08-23 14:15:38 +0100

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -11,28 +11,65 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MyApp'),
+        title: Text(
+          'MyApp',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
         backgroundColor: Colors.red[600],
         centerTitle: true,
       ),
       body: Center(
-          child: Column(children: [
-        Text(
-          'Hello World',
-          style: TextStyle(
-            fontSize: 20.0,
-            letterSpacing: 4.0,
-            color: Colors.grey[600],
-            fontFamily: 'RobotoSlab-Variable',
-          ),
-        ),
-        Image.asset('images/bmw-1.jpg'),
-        Image.asset('images/bmw-2.jpg'),
-        Image.asset('images/bmw-3.jpg'),
-      ])),
+        child: TextButton(
+            onPressed: () {
+              print('Click Me button was clicked');
+            },
+            child: ElevatedButton.icon(
+              onPressed: () {
+                print('Message button was clicked');
+              },
+              icon: Icon(
+                Icons.mail,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red[600],
+                shadowColor: Color.fromARGB(255, 227, 144, 35),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                minimumSize: Size(100, 40),
+              ),
+              label: Text('Message',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  )),
+            )
+            // child: Text('Click Me',
+            //     style: TextStyle(
+            //       fontSize: 15,
+            //       color: Colors.white,
+            //     )),
+            // style: ElevatedButton.styleFrom(
+            //   backgroundColor: Colors.red[600],
+            //   shadowColor: Color.fromARGB(255, 227, 144, 35),
+            //   shape: RoundedRectangleBorder(
+            //     borderRadius: BorderRadius.circular(10.0),
+            //   ),
+            //   minimumSize: Size(100, 20),
+            // ),
+            ),
+      ),
       floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
           onPressed: () {},
-          child: Text('Click'),
           backgroundColor: Colors.red[600]),
     );
   }
