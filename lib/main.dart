f374d104f82549eb6a81a2ef7a1509dc59e0a271
Oghenedoro David Agbroko <sirdoro1@gmail.com>
2023-12-24 17:26:46 +0100

    update on adding icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
-}
+void main() =>   runApp(MaterialApp(
+  home: Home(),
+));
 
-class MyApp extends StatelessWidget {
+class Home extends StatelessWidget {
+  // const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'MyApp',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        backgroundColor: Colors.red[600],
-        centerTitle: true,
-      ),
-      body: Column(children: <Widget>[
-        TextButton(
-            onPressed: () {
-              print('Click Me button was clicked');
-            },
-            child: ElevatedButton.icon(
-              onPressed: () {
-                print('Message button was clicked');
-              },
-              icon: const Icon(
-                Icons.mail,
-                color: Colors.white,
-              ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red[600],
-                shadowColor: const Color.fromARGB(255, 227, 144, 35),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                minimumSize: const Size(100, 40),
-              ),
-              label: const Text('Message',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
-                  )),
-            )),
-        const Text('Hello World'),
-        Container(
-          height: 100,
-          width: 200,
-          color: Colors.blue,
-          padding: const EdgeInsets.all(20.0),
-          child: const Text('Inside Container'),
-          margin: const EdgeInsets.all(2.0),
-        ),
-      ]),
-      // floatingActionButton: FloatingActionButton(
-      //     child: const Icon(
-      //       Icons.add,
-      //       color: Colors.white,
-      //     ),
-      //     onPressed: () {},
-      //     backgroundColor: Colors.red[600]),
+    appBar: AppBar(
+      title: const Text('My First Apps'),
+      backgroundColor: Colors.red[600], 
+      centerTitle: true,
+    ),
+    body: const Center(
+      child:Icon(
+        Icons.airport_shuttle,
+        color: Colors.lightBlue,
+        size: 50.0,
+      )
+      ), 
+      floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      backgroundColor: Colors.red[600],
+      child: const Icon(Icons.add),
+    ),  
     );
   }
 }
+
